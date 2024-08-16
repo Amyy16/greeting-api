15c1b637c10eec4b3aade1dd5c85515c0ce49e4e
Amarachi <nwabueze2amarachi@gmail.com>
2024-08-16 16:35:54 +0100

    checking ip

--- index.js
+++ index.js
@@ -7,36 +7,34 @@ const openweather = process.env.OPENWEATHER_API_KEY;
 
 const app = express();
 app.use(express.json());
-// app.use(requestip.mw());
 
 app.get("/api/hello", async (req, res) => {
   try {
-    const { visitor_name } = req.query || "guest";
+    const visitor_name = req.query.visitor_name || "guest";
     const ipAddress =
       req.headers["x-real-ip"] ||
       req.headers["x-forwarded-for"] ||
       req.socket.remoteAddress ||
-      "";
+      " ";
 
     console.log(ipAddress);
 
-    //fetch location
-    const response = await axios.get(
-      `http://api.ipstack.com/${ipAddress}?access_key=${ipstack}`
-    );
-    const location = response.data.city;
+    // const locationresponse = await axios.get(
+    //   `http://api.ipstack.com/${ipAddress}?access_key=${ipstack}`
+    // );
+    // const location = locationresponse.data.city;
 
-    // fetch weather using location
-    const weatherResponse = await axios.get(
-      `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${openweather}&units=metric`
-    );
-    const weather = weatherResponse.data.main.temp;
+    // // fetch weather using location
+    // const weatherResponse = await axios.get(
+    //   `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${openweather}&units=metric`
+    // );
+    // const weather = weatherResponse.data.main.temp;
 
     res.status(200).json({
       visitor_name: visitor_name,
       ip: ipAddress,
-      location: location,
-      weather: weather,
+      // location: location,
+      // weather: weather,
     });
   } catch (error) {
     res.status(500).json(error.message);
